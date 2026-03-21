3010d509b5084f2ba37c9e85d1d8f8281b465cc0
kgjun0314 <unicorn9717@gmail.com>
2026-03-21 20:05:23 +0900

    add description on MemberController

--- src/main/java/com/kgj0314/e_commerce_backend/presentation/controller/MemberController.java
+++ src/main/java/com/kgj0314/e_commerce_backend/presentation/controller/MemberController.java
@@ -5,9 +5,10 @@ import com.kgj0314.e_commerce_backend.application.service.MemberService;
 import com.kgj0314.e_commerce_backend.domain.member.Role;
 import com.kgj0314.e_commerce_backend.presentation.dto.MemberSignupRequestDto;
 import com.kgj0314.e_commerce_backend.application.dto.MemberResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -18,6 +19,12 @@ import java.net.URI;
 public class MemberController {
     private final MemberService memberService;
 
+    @Operation(
+            summary = "사용자 회원가입",
+            description = "사용자로 회원가입합니다."
+    )
+    @ApiResponse(responseCode = "201", description = "회원가입 성공")
+    @ApiResponse(responseCode = "409", description = "회원가입 실패: 중복 데이터 존재")
     @PostMapping("/as_user")
     public ResponseEntity<MemberResponseDto> signupAsUser(@RequestBody MemberSignupRequestDto memberSignupRequestDto){
         MemberSignupCommand memberSignupCommand =
@@ -31,6 +38,12 @@ public class MemberController {
         return ResponseEntity.created(URI.create("/api/members/" + memberResponseDto.getId())).body(memberResponseDto);
     }
 
+    @Operation(
+            summary = "관리자 회원가입",
+            description = "관리자로 회원가입합니다."
+    )
+    @ApiResponse(responseCode = "201", description = "회원가입 성공")
+    @ApiResponse(responseCode = "409", description = "회원가입 실패: 중복 데이터 존재")
     @PostMapping("/as_admin")
     public ResponseEntity<MemberResponseDto> signupAsAdmin(@RequestBody MemberSignupRequestDto memberSignupRequestDto){
         MemberSignupCommand memberSignupCommand =
@@ -44,7 +57,13 @@ public class MemberController {
         return ResponseEntity.created(URI.create("/api/members/" + memberResponseDto.getId())).body(memberResponseDto);
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(
+            summary = "ID로 회원 조회",
+            description = "memberId로 회원을 조회합니다."
+    )
+    @ApiResponse(responseCode = "200", description = "회원 조회 성공")
+    @ApiResponse(responseCode = "403", description = "로그인이 필요합니다.")
+    @ApiResponse(responseCode = "404", description = "존재하지 않는 사용자입니다.")
     @GetMapping("/{id}")
     public ResponseEntity<MemberResponseDto> getMember(@PathVariable Long id) {
         MemberResponseDto memberResponseDto = memberService.getMember(id);
